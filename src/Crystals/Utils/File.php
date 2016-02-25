f23a3af4451a833817f8ec1218cc16b496490b18
Sergey Novikov <novikov.stranger@gmail.com>
2016-02-26 01:48:00 +0600

    Add getter for file resource

--- src/Crystals/Utils/File.php
+++ src/Crystals/Utils/File.php
@@ -104,4 +104,12 @@ class File
     {
         return $this->filename;
     }
+
+    /**
+     * @return resource
+     */
+    public function getResource()
+    {
+        return $this->resource;
+    }
 }
